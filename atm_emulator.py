3bbd01e21ff75af2543bfec268596e6026cccc50
lh77x7 <75083883+lh77x7@users.noreply.github.com>
2020-12-16 11:41:51 +0100

    Update atm_emulator.py

--- atm_emulator.py
+++ atm_emulator.py
@@ -1,7 +1,8 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 import os
 import string
 import getpass
+import time
 
 #create list of users, their PINs and bank statements
 #here - version of 5 users list, but ATMs connects with database and then
@@ -11,25 +12,27 @@ users = ['krzysiek', 'denis', 'tomek', 'peter', 'lukas']
 pin_code = ['1111', '2222', '3333', '4444', '5555']
 amount = [500, 1000, 1500, 2000, 2500]
 count = 0
+inner_counter = 0
 
 #check if user exists
 while True:
-    user = input('\nENTER USER NAME: ')
-    user = user.lower()
-    if user in users:
-        if user == users[0]:
-            n = 0
-        elif user == users[1]:
-            n = 1
-        elif user == users[2]:
-            n = 2
-        elif user == users[3]:
-            n = 3
+        user = input('\nENTER USER NAME: ')
+        user = user.lower()
+        if user in users:
+            if user == users[0]:
+                n = 0
+            elif user == users[1]:
+                n = 1
+            elif user == users[2]:
+                n = 2
+            elif user == users[3]:
+                n = 3
+            else:
+                n = 4
+            break
         else:
-            n = 4
-        break
-    else:
-        print('-----------------------')
+            #wrong username, 5 attemps counting
+            print('-----------------------')
         print('WRONG USERNAME')
         print('-----------------------')
 
@@ -94,3 +97,44 @@ while count < 3:
         print('PIN CODE HAS 4 DIGITS')
         print('----------------------')
         count = count + 1
+
+#while user has 3 false attempts
+while count == 3:
+        print('----------------------')
+        print('YOUR CARD IS LOCKED')
+        print('3 FALSE ATTEMPTS')
+        print('----------------------')
+        exit()
+
+print('-------------------')
+print('WELCOME TO YOUR ACCOUNT ', str.capitalize(users[n]))
+print('-------------------')
+while True:
+    print('-----------------------')
+    options = input('SELECT OPTION:\nStatement-(S), Withdraw-(W) \n'
+    'Cash-In-(C), Change PIN-(P) \nQuit-(Q) \n').lower()
+    print('-----------------------')
+    valid_options = ['s', 'w', 'c', 'p', 'q']
+    options = options.lower()
+    if options == 's':
+       print('-------------------')
+       print(str.capitalize(users[n]), ', MONEY STATUS: ', amount[n], 'EURO. ')
+       print('-------------------')
+    elif options == 'w':
+      print('--------------------')
+      print('w otions')
+      print('--------------------')
+    elif options == 'c':
+      print('--------------------')
+      print('c options')
+      print('--------------------')
+    elif options == 'p':
+      print('--------------------')
+      print('p otions')
+      print('--------------------')
+    elif options == 'q':
+        exit()
+    else:
+        print('------------------')
+        print('OPTIONS NOT VALID')
+        print('------------------')
